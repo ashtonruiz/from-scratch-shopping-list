dd7d007d8dff28a26ac749b7bf085030a8bbc2c4
Ashton Ruiz <ashton.ruiz@yahoo.com>
2022-11-29 12:00:43 -0800

    added CSS! Almost done with the project - everything looks good. However I did just click one of my grocery items for the first time and the function is working as it should but visually it's not very accessible because the item will just move to the bottom of the list. I want something visually to happen - I want to see if I can figure that out and then I'll finish up CSS.

--- render-utils.js
+++ render-utils.js
@@ -1,5 +1,5 @@
 export function renderListItem(itemObject) {
     const listItemEl = document.createElement('li');
-    listItemEl.textContent = `${itemObject.item}: ${itemObject.quantity}/10`;
+    listItemEl.textContent = `${itemObject.item}: ${itemObject.quantity}`;
     return listItemEl;
 }
